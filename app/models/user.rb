2e0268ef418a5eb08da2b7fe177e5812fd3f87e4
MusaAbdillah <musaabdillahm9@gmail.com>
2017-10-16 12:15:51 +0700

    adding litle comment on model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	#association 
+	#association between model using polymorphism
 	has_many :microposts, dependent: :destroy
 
 	has_many :active_relationships, class_name: "Relationship",
